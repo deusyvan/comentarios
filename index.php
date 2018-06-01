4809ecd2b618bc842510c96cbfb2e731b9c6bdfd
deusyvan <deusyvan@gmail.com>
2018-06-01 09:34:54 -0300

    Exemplificando como vai ficar a msg

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <fieldset>
-
 	<form method="POST">
+		Nome: <br/>
+		<input type="text" name="nome"><br/><br/>
 		
+		Mensagem:<br/>
+		<textarea name="mensagem"></textarea><br/><br/>
+		
+		<input type="submit" value="Enviar Mensagem" />
 	</form>
-
 </fieldset>
+
+<strong>Nome da pessoa</strong><br/>
+A mensagem fica aqui
+
