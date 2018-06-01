8d68fd33cbb0f6b931263ca0475d6e42b95490e4
deusyvan <deusyvan@gmail.com>
2018-06-01 09:38:46 -0300

    Vamos mostrar as mensagens do banco, então faremos a conexão

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+try {
+    $pdo = new PDO("mysql:dbname=blog;host=localhost","admin","admin");
+} catch (PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
+
+?>
+
 <fieldset>
 	<form method="POST">
 		Nome: <br/>
